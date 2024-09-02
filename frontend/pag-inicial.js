8cd93fce30e4b1cc888095fe03eb0d1f026e0e9b
Hinhameh <rryyaann752@hotmail.com>
2024-09-02 18:22:47 -0300

    adicionei o sidebar do perfil e fiz umas transicoes neles

--- frontend/pag-inicial.js
+++ frontend/pag-inicial.js
@@ -1,12 +1,27 @@
 function toggleSidebar() {
     var sidebar = document.getElementById("menu-sidebar");
-    sidebar.classList.toggle("open"); 
+    sidebar.classList.toggle("open");
 }
+
+function sidebarPerfil() {
+    var sidebarPerfil = document.getElementById("perfil-sidebar");
+    sidebarPerfil.classList.toggle("open");
+}
+
+// Evento de clique unificado para fechar os sidebars ao clicar fora
 document.addEventListener('click', function(event) {
-    var sidebar = document.getElementById("menu-sidebar");
+    var menuSidebar = document.getElementById("menu-sidebar");
+    var perfilSidebar = document.getElementById("perfil-sidebar");
     var menuButton = document.querySelector('.menu-botao');
-    
-    if (!sidebar.contains(event.target) && !menuButton.contains(event.target)) {
-        sidebar.classList.remove('open');
+    var perfilButton = document.getElementById("perfil");
+
+    // Verifica se o clique foi fora do menu lateral
+    if (menuSidebar.classList.contains('open') && !menuSidebar.contains(event.target) && !menuButton.contains(event.target)) {
+        menuSidebar.classList.remove('open');
+    }
+
+    // Verifica se o clique foi fora do sidebar do perfil
+    if (perfilSidebar.classList.contains('open') && !perfilSidebar.contains(event.target) && !perfilButton.contains(event.target)) {
+        perfilSidebar.classList.remove('open');
     }
 });
