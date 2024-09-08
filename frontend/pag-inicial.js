0dfb1ceee366aad669208b1c093d3813d662963d
Hinhameh <rryyaann752@hotmail.com>
2024-09-08 00:04:08 -0300

    tentando fazer o maldito carrossel, add tbm o "teste.html", acho q o nome é auto-explicativo

--- frontend/pag-inicial.js
+++ frontend/pag-inicial.js
@@ -1,19 +1,19 @@
 function toggleSidebar() {
-    var sidebar = document.getElementById("menu-sidebar");
+    const sidebar = document.getElementById("menu-sidebar");
     sidebar.classList.toggle("open");
 }
 
 function sidebarPerfil() {
-    var sidebarPerfil = document.getElementById("perfil-sidebar");
+    const sidebarPerfil = document.getElementById("perfil-sidebar");
     sidebarPerfil.classList.toggle("open");
 }
 
 // Evento de clique unificado para fechar os sidebars ao clicar fora
 document.addEventListener('click', function(event) {
-    var menuSidebar = document.getElementById("menu-sidebar");
-    var perfilSidebar = document.getElementById("perfil-sidebar");
-    var menuButton = document.querySelector('.menu-botao');
-    var perfilButton = document.getElementById("perfil");
+    const menuSidebar = document.getElementById("menu-sidebar");
+    const perfilSidebar = document.getElementById("perfil-sidebar");
+    const menuButton = document.querySelector('.menu-botao');
+    const perfilButton = document.getElementById("perfil");
 
     // Verifica se o clique foi fora do menu lateral
     if (menuSidebar.classList.contains('open') && !menuSidebar.contains(event.target) && !menuButton.contains(event.target)) {
