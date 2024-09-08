662947891ed4e9c2a845857ae2a88f56e730e79d
Hinhameh <rryyaann752@hotmail.com>
2024-09-08 19:43:11 -0300

    desisto da vida

--- frontend/pag-inicial.js
+++ frontend/pag-inicial.js
@@ -2,7 +2,7 @@
 function toggleSidebar() {
     const sidebar = document.getElementById("menu-sidebar");
     sidebar.classList.toggle("open");
-    updateEsponsividade(sidebar.classList.contains("open"));
+    atualizaresponsividade(sidebar.classList.contains("open"));
 }
 
 function sidebarPerfil() {
@@ -12,9 +12,9 @@ function sidebarPerfil() {
 }
 
 // Função para atualizar a classe 'active' no elemento #esponsividade
-function updateEsponsividade(isActive) {
+function atualizaresponsividade(ativo) {
     const esponsividade2 = document.querySelector("#esponsividade");
-    if (isActive) {
+    if (ativo) {
         esponsividade2.classList.add("active");
     } else {
         esponsividade2.classList.remove("active");
@@ -31,12 +31,17 @@ document.addEventListener('click', function(event) {
     // Verifica se o clique foi fora do menu lateral
     if (menuSidebar.classList.contains('open') && !menuSidebar.contains(event.target) && !menuButton.contains(event.target)) {
         menuSidebar.classList.remove('open');
-        updateEsponsividade(false); // Remove a classe 'active' ao fechar
+        atualizaresponsividade(false); // Remove a classe 'active' ao fechar
     }
 
     // Verifica se o clique foi fora do sidebar do perfil
     if (perfilSidebar.classList.contains('open') && !perfilSidebar.contains(event.target) && !perfilButton.contains(event.target)) {
         perfilSidebar.classList.remove('open');
-        updateEsponsividade(false); // Remove a classe 'active' ao fechar
+         // Remove a classe 'active' ao fechar
     }
 });
+function Carrossel(){
+    document.addEventListener('click')
+    const Carrossel= document.getElementById("carrossel")
+    
+};
