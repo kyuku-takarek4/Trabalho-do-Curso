64302f934538eb55027088ecf7d2bc94218118be
rafael <rafaelsiqueirapimentel00@gmail.com>
2024-09-12 21:24:38 -0300

    Últimos detalhes do Firebase pra você seu merdinhas

--- BackEnd/config/configFirebase.js
+++ BackEnd/config/configFirebase.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+var serviceAccount = require("./secrets/programinhos-programantes-firebase-adminsdk-aprh8-172ea43215.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
