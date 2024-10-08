3dcedacc8fb287e543c91be3fa5410169a7270ab
rafael <rafaelsiqueirapimentel00@gmail.com>
2024-10-08 18:57:36 -0300

    Mundanças básicas

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 
 // Configuração do Firebase
 const admin = require('firebase-admin');
-const serviceAccount = require('../secrets/banco-de-bagos-firebase-adminsdk-ao45g-72977226b0.json');
+const serviceAccount = require('../secrets/banco-de-bagos-firebase-adminsdk-ao45g-6bbf43d49e.json');
 
 // Middlewares
 app.use(express.json());
