ebac1f63907ced218729af8651de87e8a66d02ef
Hinhameh <rryyaann752@hotmail.com>
2024-10-15 17:38:07 -0300

    eliminei o perfil-sidebar

--- frontend/pag-inicial.js
+++ frontend/pag-inicial.js
@@ -5,12 +5,6 @@ function toggleSidebar() {
     atualizaresponsividade(sidebar.classList.contains("open"));
 }
 
-function sidebarPerfil() {
-    const sidebarPerfil = document.getElementById("perfil-sidebar");
-    sidebarPerfil.classList.toggle("open");
-  
-}
-
 
 function atualizaresponsividade(ativo) {
     const esponsividade2 = document.querySelector("#esponsividade");
@@ -24,19 +18,12 @@ function atualizaresponsividade(ativo) {
 
 document.addEventListener('click', function(event) {
     const menuSidebar = document.getElementById("menu-sidebar");
-    const perfilSidebar = document.getElementById("perfil-sidebar");
     const menuButton = document.querySelector('.menu-botao');
     const perfilButton = document.getElementById("perfil");
 
     if (menuSidebar.classList.contains('open') && !menuSidebar.contains(event.target) && !menuButton.contains(event.target)) {
         menuSidebar.classList.remove('open');
         atualizaresponsividade(false);
-    }
-
-
-    if (perfilSidebar.classList.contains('open') && !perfilSidebar.contains(event.target) && !perfilButton.contains(event.target)) {
-        perfilSidebar.classList.remove('open');
-     
     }
 });
 const carrossel = document.querySelector('.carrossel');
